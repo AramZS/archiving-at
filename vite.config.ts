a8e85bee0f9dec34818a6534484be6f197349db5
Aram Zucker-Scharff <Aram.Zucker-Scharff@washpost.com>
2026-05-05 17:44:40 -0400

    activate the static folder

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 export default defineConfig({
   plugins: [svelte()],
+  publicDir: "static",
   server: {
     host: "127.0.0.1",
     // Redirect all unknown paths to index.html so the SPA router handles them
