30e36b6cb74255cd29b98ab3c63ba4267a09ee3a
Aram Zucker-Scharff <aramzs@hacktext.com>
2026-05-05 18:07:46 -0400

    Serve the correct folder with the permissions doc

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 export default defineConfig({
+  publicDir: "static",
   plugins: [svelte()],
   server: {
     host: "127.0.0.1",
