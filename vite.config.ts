11d4eb7c910e27f2540b5af2c114671eaf499515
Aram Zucker-Scharff <Aram.Zucker-Scharff@washpost.com>
2026-05-06 10:36:38 -0400

    Replicated due to merge issue

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 export default defineConfig({
   publicDir: "static",
   plugins: [svelte()],
-  publicDir: "static",
   server: {
     host: "127.0.0.1",
     // Redirect all unknown paths to index.html so the SPA router handles them
